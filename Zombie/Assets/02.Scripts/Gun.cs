91530e6923ba590a83de73382a11fb388a3d5eec
이승헌 <rltmsla5@naver.com>
2022-04-19 15:53:32 +0900

    0419
    0419

--- Zombie/Assets/02.Scripts/Gun.cs
+++ Zombie/Assets/02.Scripts/Gun.cs
@@ -15,7 +15,7 @@ public class Gun : MonoBehaviour
     }
     public State state { get; private set; } //현재 총의 상태
 
-    public Transform fieTransform; //탄알이 발사될 위치
+    public Transform fireTransform; //탄알이 발사될 위치
 
     public ParticleSystem muzzleFlashEffext; // 총구 화염 효과
     public ParticleSystem shellEjectEffext; // 탄피 배출 효과
@@ -30,9 +30,11 @@ public class Gun : MonoBehaviour
 
     public int ammoRemain = 100; // 남의 전체 탄알
 
-    public int magammo; // 현재 탄창에 남아 있는 탄알
+    public int magAmmo; // 현재 탄창에 남아 있는 탄알
 
     private float lastFireTime; // 총을 마지막으로 발사한 시점
+
+    
 
     private void Awake()
         //사용할 컴포넌트의 참조 가져오기
@@ -54,7 +56,7 @@ public class Gun : MonoBehaviour
         // 전체 예비 탄알 양을 초기화
         ammoRemain = gunData.startAmmoRemain;
         // 현재 탄창을 가득 채우기
-        magammo = gunData.magCapacity;
+        magAmmo = gunData.magCapacity;
 
         // 총의 현재 상태를 총을 쏠 준비가 된 상태로 변경
         state = State.Ready;
@@ -65,17 +67,66 @@ public class Gun : MonoBehaviour
     public void Fire()
         //발사 시도
     {
-
+        // 현재상태가 발사 가능한 상태
+        // &&마지막 총 발사 시점에서 gunData.timeBetFire 이상의 시간이 지남
+        if(state==State.Ready&&Time.time>=lastFireTime+gunData.timeBetFire)
+        {
+            //마지막 총 발사 시점 갱신
+            lastFireTime = Time.time;
+            // 실제 발사 처리 싱행
+            Shot();
+        }
     }
+    
 
     private void Shot()
         //실제 발사 처리
     {
+        // 레이캐스트에 의한 충돌 정보를 저장하는 컨데이너
+        RaycastHit hit;
 
+        // 탄알이 맞은 곳을 저장할 변수
+        Vector3 hitPosition = Vector3.zero;
+
+        //레이캐스트(시작 지접,방향,충돌 정보 컨테이너,사정거리
+        if (Physics.Raycast(fireTransform.position, fireTransform.forward, out hit, fireDistance))
+        {//레일케스트는 무조건 아웃 으로 표현해야한다 로 생각하자
+            //레이가 어떤 물체와 충돌한 경우
+
+            //충돌한 상대방으로부터 IDamageable 오브젝트 가져오기 시도
+            IDamageable target = hit.collider.GetComponent<IDamageable>();
+
+            if (target != null)
+            {
+                //상대방의 OnDamage 함수를 실행시켜 상대방에 대미지 주기
+                target.OnDamage(gunData.dagame, hit.point, hit.normal);
+                //                               위치       방향
+            }
+            //레이가 충돌한 위치 저장
+            hitPosition = hit.point;
+        }
+        else
+        {
+            //레이가 다른 물체와 충돌하지 않았다면
+            //탄알이 최대 사정거리까지 날아깟을 때의 위치를 충돌 위치로 사용
+            hitPosition = fireTransform.position + fireTransform.forward * fireDistance;
+
+        }
+            // 발사 이펙트 재생시작
+            StartCoroutine(shotEffect(hitPosition));
+
+            //남은 탄알 수를 -1
+            magAmmo--;
+            if (magAmmo <= 0)
+            {
+                // 탄창에 남은 탄알이 없다면 총의 현재 상태를 Empty로 갱신
+                state = State.Empty;
+            }
+        
     }
 
     //발사 이펙트와 소리를 재생하고 탄알 궤적을 그림
-    private IEnumerator ShotEffect(Vector3 hitPosition)
+    private IEnumerator shotEffect(Vector3 hitPosition)
     //IEnumerator(코르틴메서드) 인보크라는 지연 방법도 존재 하단에 첨부
     {
         // 총구 화염 효과 재생
@@ -89,7 +140,7 @@ public class Gun : MonoBehaviour
         //플레이어 온샷은 사운들르 중첩이 가능
 
         // 선의 시작점은 총구의 위치
-        bulletLineRenderer.SetPosition(0, fieTransform.position);
+        bulletLineRenderer.SetPosition(0, fireTransform.position);
 
         // 선의 끝점은 입력으로 들어온 충돌 위치
         bulletLineRenderer.SetPosition(1, hitPosition);
